f0d48584152bfff60b0082e31f2a78787d6087c7
Joao Guilherme Tasca Pereira <adm.joaoguilherme@gmail.com>
2023-03-27 00:32:07 -0300

    made transactions table more responsive

--- static/transactions.js
+++ static/transactions.js
@@ -2,24 +2,22 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Select all the table cells in the "VALUE" column
     var cells = document.querySelectorAll("td:nth-child(4)");
+    // Convert NodeList object into Array and slice off the first element
     var cells = Array.from(cells).slice(1);
-
     // Extract the numerical values from the cells
     var values = extractValuesFromCells(cells);
-
     // Calculate the sum of the values
     var sum = calculateSum(values);
-
     // Create a new div element to display the sum
     var balanceDiv = document.createElement("div");
     balanceDiv.textContent = "Balance: " + sum;
     balanceDiv.style.fontSize = "120%";
     balanceDiv.style.display = "flex";
     balanceDiv.style.justifyContent = "right";
-
     // Append the div to a container element after the table
     var tableCard = document.querySelector(".tableCard");
     tableCard.appendChild(balanceDiv);
+
 
     $(".tablesorter").tablesorter({
         headers: {
@@ -30,13 +28,52 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+
+
     var toggleButtons = document.querySelectorAll(".toggle_button");
     toggleButtons.forEach(function(toggleButton) {
-        toggleButton.addEventListener("click", showElement);
+        toggleButton.addEventListener("click", function() {
+            var hiddenElementId = this.getAttribute("hiddenElementId");
+            var hiddenElement = document.getElementById(hiddenElementId);
+            hiddenElement.classList.toggle("hidden");
+            if (hiddenElement.classList.contains("hidden")) {
+                hiddenElement.style.display = "";
+                hiddenElement.style.flexDirection = "";
+            }
+            else {
+                if (hiddenElementId == "formRow") {
+                    if (viewportWidth <= 700) {
+                        hiddenElement.style.display = "flex";
+                        hiddenElement.style.flexDirection = "column";
+                    }
+                    else
+                    {
+                        hiddenElement.style.display = "";
+                        hiddenElement.style.flexDirection = "";
+                    }
+
+                }
+            }
+        });
     });
+
+    var viewportWidth = window.innerWidth;
+    var formRow = document.getElementById("formRow")
+    window.onresize = function() {
+    viewportWidth = window.innerWidth;
+    if  (!(formRow.classList.contains("hidden"))) {
+        if (viewportWidth <= 700) {
+            formRow.style.display = "flex";
+            formRow.style.flexDirection = "column";
+        }
+        else {
+            formRow.style.display = "";
+            formRow.style.flexDirection = "";
+        }
+    }
+
+}
 });
-
-
 
 
 /**
@@ -44,9 +81,7 @@ document.addEventListener("DOMContentLoaded", function() {
  * @param {Event} event - The event object.
  */
 function showElement() {
-    var hiddenElementId = this.getAttribute("hiddenElementId");
-    var hiddenElement = document.getElementById(hiddenElementId);
-    hiddenElement.classList.toggle("hidden");
+
 }
 
 
